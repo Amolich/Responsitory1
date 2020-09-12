696b391f5fe3c4a958a140c7683d449dcd44ca01
Amolich <835416252@qq.com>
2020-09-12 08:37:44 +0800

    comments

--- src/view/home.vue
+++ src/view/home.vue
@@ -35,40 +35,6 @@
           page_change(p)
           {
             this.page = p;
-          },
-          test()
-          {
-            var L="一二三四五六七八九十"
-            var N= 3
-            var ans = ""
-            var count = 0
-            if(L.length <= N)
-            {
-              ans = L.substring(0,N);
-            }
-            else
-            {
-              for (var i = 0;count < N-3; i++)
-              {
-                var charCode = L.charAt(i);
-                var reg = new RegExp("[\\u4E00-\\u9FFF]+","g");
-                if (!reg.test(charCode))
-                {
-                  ans = ans + charCode
-                  count += 1
-                }
-                else
-                {
-                  ans = ans + charCode
-                  count += 2
-                }
-
-              }
-              if(count > N-3)
-                ans = ans.substr(0, ans.length - 1);
-              ans = ans+"..."
-            }
-            alert(ans)
           }
         }
     }
